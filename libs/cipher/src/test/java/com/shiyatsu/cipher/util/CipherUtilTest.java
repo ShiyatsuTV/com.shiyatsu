b6b8d4d220cf16dcf68d62c4d7ba32a5daac3a20
ShiyatsuTV <shiyatsu70@gmail.com>
2024-10-22 00:15:42 +0200

    Sonar smell code

--- libs/cipher/src/test/java/com/shiyatsu/cipher/util/CipherUtilTest.java
+++ libs/cipher/src/test/java/com/shiyatsu/cipher/util/CipherUtilTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class CipherUtilTest {
+class CipherUtilTest {
 
     @Test
     void testEncryptDecrypt() throws Exception {
